802e3847ff2b15d57692a1de17adbef23e792cd9
BrendaMwiza <brendabrizy@gmail.com>
2019-10-27 18:30:31 +0200

    added first view

--- award/views.py
+++ award/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    # date = dt.date.today()
+    image_pic = Projects.objects.all()
+
+    return render(request, 'index.html', {"image_pic":image_pic})
